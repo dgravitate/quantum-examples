46645645a5da8fbff3eab5c3147feb04d9c0c1af
David Graves <dgraves@thesummitgrp.com>
2025-08-27 21:30:22 -0500

    add docstrings

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,22 @@
+"""
+A module for preparing, optimizing, and executing quantum circuits on the IBM Quantum
+platform using Qiskit Runtime.
+
+The module provides functions to save the IBM Quantum Runtime API credentials, optimize
+quantum circuits using a preset pass manager, and execute them on the least busy backend
+available while handling data simulation efficiently.
+
+This module demonstrates creating a basic 1-qubit quantum circuit, applying
+a Hadamard gate to the qubit to achieve a superposition state, and measuring
+the circuit's output
+
+Functions:
+    get_service: Saves and retrieves the Qiskit Runtime service instance configured
+        with IBM Quantum API credentials.
+    execute: Optimizes, compiles, and runs the provided Quantum Circuit on the least
+        busy quantum backend. Outputs the measurement results.
+"""
+
 from qiskit import QuantumCircuit
 from qiskit_ibm_runtime.sampler import SamplerV2 as Sampler
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
--- hello_world_simulated.py
+++ hello_world_simulated.py
@@ -1,3 +1,21 @@
+"""
+This module provides functionality to simulate and run a simple quantum circuit
+using IBM's Qiskit framework. The module includes methods to obtain a quantum
+computation service, execute a quantum circuit using a sampler, and set up a
+basic quantum circuit for simulation.
+
+This module demonstrates creating a basic 1-qubit quantum circuit, applying
+a Hadamard gate to the qubit to achieve a superposition state, and measuring
+the circuit's output.
+
+Functions:
+    get_service: Returns an instance of a quantum computation service to be
+        used for simulation.
+
+    execute: Executes a given quantum circuit on the provided service with
+        a specified number of shots.
+"""
+
 from qiskit import QuantumCircuit
 from qiskit_aer import AerSimulator
 from qiskit_ibm_runtime.sampler import SamplerV2 as Sampler
